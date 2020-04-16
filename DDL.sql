fd815fac900904230d2c2a676347984f46236c59
Jeff Winchell <Jeff_Winchell@g.Harvard.edu>
2020-04-16 12:26:40 -0400

    SQL and Excel code updates

--- DDL.sql
+++ DDL.sql
@@ -12,28 +12,37 @@ Go
 Create FullText Catalog MSM With Accent_Sensitivity = On As Default
 Go
 Create Table Media_People(
-	[Name] VarChar(20) Not Null Constraint PK_Media_People Primary Key
+	[Name] VarChar(30) Not Null Constraint PK_Media_People Primary Key
 	)
 Go
 Create Table Show(
-	Show VarChar(30) Not Null Constraint PK_Show Primary Key,
+	Show VarChar(40) Not Null Constraint PK_Show Primary Key,
 	Network VarChar(5) Not Null Constraint CK_Network Check(Network In ('MSNBC','CNN','FN','NBC','CBS','ABC','NPR','PBS')),
-	Host VarChar(20) Not Null Constraint FK_Show_Media_People Foreign Key References Media_People
+	Host VarChar(30) Not Null Constraint FK_Show_Media_People Foreign Key References Media_People
 	)
 Go
 Create Index Host On Show(Host)
 Go
-Create Table Transcript (
-	Id Int Not Null Constraint PK_Transcript Primary Key,
-	[Text] VarChar(Max) Collate LATIN1_GENERAL_100_CI_AS_SC_UTF8 Not Null,
-	Show VarChar(30) Not Null Constraint FK_Transcript_Show Foreign Key References Show,
-	AiredOn DateTimeOffset(0) Not Null,
-	Guest_Host VarChar(20) Null Constraint FK_Transcript_Media_People Foreign Key References Media_People,
-	Constraint U_Transcript Unique(Show,AiredOn)
+Create Table Episode (
+	Show VarChar(40) Not Null Constraint FK_Episode_Show Foreign Key References Show,
+	AiredOn DateTime Not Null,
+	Guest_Host VarChar(30) Null Constraint FK_Episode_Media_People Foreign Key References Media_People,
+	Constraint PK_Episode Primary Key(Show,AiredOn)
 	)
 Go
-Create Index Show On Transcript(Show)
-Create Index Guest_Host On Transcript(Guest_Host)
+Create Index Show On Episode(Show)
+Create Index Guest_Host On Episode(Guest_Host)
+Go
+Create Table Transcript (
+	Id Int Not Null Identity Constraint PK_Transcript Primary Key,
+	Show VarChar(40) Not Null,
+	AiredOn DateTime Not Null,
+	[Text] VarChar(Max) Collate LATIN1_GENERAL_100_CI_AS_SC_UTF8 Not Null,
+	Speaker VarChar(30) Not Null Constraint FK_Speaker_Media_People Foreign Key References Media_People,
+	Constraint FK_Transcript_Episode Foreign Key (Show,AiredOn) References Episode
+	)
+Create Index FK_Transcript_Episode On Transcript(Show,AiredOn)
+
 Create FullText Index
 	On Transcript(
 		[Text] Language English Statistical_Semantics)
@@ -41,3 +50,11 @@ Create FullText Index
 Go
 Alter Fulltext Index On Transcript Start Full Population;
 Go
+Create Or Alter Procedure Add_Transcript_Text(@Show VarChar(40),@AiredOn VarChar(19),@Speaker VarChar(30),@Text VarChar(Max))
+As Begin
+	If Not Exists(Select * From Media_People Where [Name]=@Speaker)
+		Insert Into Media_People ([Name]) Values (@Speaker)
+	If Not Exists(Select * From Episode Where Episode.Show=@Show And Episode.AiredOn=Convert(DateTime,@AiredOn,120))
+		Insert Into Episode (Show,AiredOn) Values (@Show,@AiredOn)
+	Insert Into Transcript (Show,AiredOn,Speaker,[Text]) Values (@Show,Convert(DateTime,@AiredOn,120),@Speaker,@Text)
+End
---
+++ MSNBC_Dem_Speaking_Time(Words).sql
@@ -0,0 +1,58 @@
+Declare @FirstDate Date = '2019-01-01',@LastDate Date = GetDate();
+With CTE AS (
+Select FromDate From
+(
+Select Top (DateDiff(Day, @FirstDate, @LastDate) + 1)
+    FromDate = DateAdd(Day, Row_Number() Over(Order By A.Object_Id) - 1,@FirstDate)
+From Sys.All_Objects a
+    Cross Join sys.All_Objects b
+) Temp 
+Where DateName(Weekday,FromDate) not in ('Saturday','Sunday')
+)
+Select FromDate As [Date],
+		Coalesce(Summary.Bernie_Words,0) As Bernie_Words,
+		Cast(100.*Coalesce(Summary.Bernie_Words,0)/Summary.All_Words As Numeric(5,2)) As Percent_Bernie_Speaking,
+		Cast(100.*Coalesce(Summary.Biden_Words,0)/Summary.All_Words As Numeric(5,2)) As Percent_Biden_Speaking,
+		Cast(100.*Coalesce(Summary.Bloomberg_Words,0)/Summary.All_Words As Numeric(5,2)) As Percent_Bloomberg_Speaking,
+		Cast(100.*Coalesce(Summary.Warren_Words,0)/Summary.All_Words As Numeric(5,2)) As Percent_Warren_Speaking,
+		Cast(100.*Coalesce(Summary.Pete_Words,0)/Summary.All_Words As Numeric(5,2)) As Percent_Pete_Speaking,
+		Cast(100.*Coalesce(Summary.Kamala_Words,0)/Summary.All_Words As Numeric(5,2)) As Percent_Kamala_Speaking
+	From CTE
+			Left Outer Join
+		(Select [Date],Sum(Bernie_Words) As Bernie_Words,Sum(Biden_Words) As Biden_Words,Sum(Warren_Words) As Warren_Words,Sum(Pete_Words) As Pete_Words,Sum(Bloomberg_Words) As Bloomberg_Words,Sum(Kamala_Words) As Kamala_Words,Sum(All_Words) As All_Words
+			From (Select Cast(AiredOn As Date) As [Date],
+						Case When Speaker In ('JUNIOR SEN. BERNIE SANDERS','BERNIE SANDERS','SEN BERNIE SANDERS','SEN. BERNIE SANDER','SEN.BERNIE SANDERS','SEN. BERNIE SANDERS','SENATOR BERNIE SANDERS') 
+							Then 1 + Len([Text]) - Len(Replace([Text],' ',''))
+							Else 0 
+						End As Bernie_Words,
+						Case When Speaker In ('FMR. SEN. JOE BIDEN','J. BIDEN','JOE BIDEN','JOE BIDEN. FORMER VICE PRESIDE','JOSEPH BIDEN','SEN. JOE BIDEN','THEN-SENATOR JOE BIDEN','VICE PRES. JOE BIDEN','VICE PRESIDENT JOSEPH BIDEN')
+							Then 1 + Len([Text]) - Len(Replace([Text],' ',''))
+							Else 0 
+						End As Biden_Words,
+						Case When Speaker In ('ELIZABETH WARREN','NONE:SEN.WARREN','PROF. ELIZABETH WARREN','PROFESSOR ELIZABETH WARREN','REP. ELIZABETH WARREN','SEN ELIZABETH WARREN','SEN. ELIZABETH WARREN','SEN. WARREN','SENATOR ELIZABETH WARREN')
+							Then 1 + Len([Text]) - Len(Replace([Text],' ',''))
+							Else 0 
+						End As Warren_Words,
+						Case When Speaker In ('FMR. MAYOR PETE BUTTIGIEG','MAYOR PETE BUTIGIEG','MAYOR PETE BUTTIGIEG','MAYOR PETER BUTTIGIEG','PETE BUTTIGIEG','PETER BUTTIGIEG','NONE:BUTTIEGIEG')
+							Then 1 + Len([Text]) - Len(Replace([Text],' ',''))
+							Else 0 
+						End As Pete_Words,
+						Case When Speaker In ('MICHAEL BLOOMBER','MICHAEL BLOOMBERG','MIKE BLOOMBERG','NONE:BLOOMBERG')
+							Then 1 + Len([Text]) - Len(Replace([Text],' ',''))
+							Else 0 
+						End As Bloomberg_Words,
+						Case When Speaker In ('KAMALA HARRIS','SEN. KAMAL HARRIS','SEN. KAMALA DEVI HARRIS','SEN. KAMALA HARRIS','SENATOR KAMALA HARRIS')
+							Then 1 + Len([Text]) - Len(Replace([Text],' ',''))
+							Else 0 
+						End As Kamala_Words,
+						1 + Len([Text]) - Len(Replace([Text],' ','')) As All_Words
+					From Transcript 
+					Where AiredOn >= '2019-01-01'
+				) Temp 
+			Group By [Date]
+		) Summary
+				On Summary.[Date]=CTE.FromDate
+	Where DateName(Weekday, CTE.FromDate) Not In ('Saturday','Sunday') 
+		And Summary.All_Words Is Not Null
+	Order By 1T
+	Option (MaxRecursion 370)
